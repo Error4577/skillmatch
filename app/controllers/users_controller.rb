2a8d558a3502289a863df9bdbb1672f816f2b43e
Error4577 <donttreadonme4577@gmail.com>
2019-11-06 20:39:37 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
   def index
-    @users = User.where.not(id: current_user.id)
+    if current_user.corporation == "個人"
+      @users = User.where(corporation: 1)
+    elsif current_user.corporation == "法人"
+      @users = User.where(corporation: 0)
+    end
   end
-  
+
   def show
     @user = User.find_by(id: params[:id])
   end
