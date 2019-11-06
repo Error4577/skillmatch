9a63b13352c4d2a25aae04e1b871e0dbebd54e20
Error4577 <donttreadonme4577@gmail.com>
2019-11-06 20:57:14 +0900

    set association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :user_skills
+  has_many :skills, through: :user_skills
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   mount_uploader :img_name, ImgNameUploader
