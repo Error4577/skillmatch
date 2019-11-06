2f67bdd8a547b40295639083b427ebc85f93836b
Error4577 <donttreadonme4577@gmail.com>
2019-11-06 20:50:38 +0900

    Update 20191106114747_create_skills.rb

--- db/migrate/20191106114747_create_skills.rb
+++ db/migrate/20191106114747_create_skills.rb
@@ -1,7 +1,7 @@
 class CreateSkills < ActiveRecord::Migration[5.2]
   def change
     create_table :skills do |t|
-
+      t.string  :name, null: false, unique: true
       t.timestamps
     end
   end
