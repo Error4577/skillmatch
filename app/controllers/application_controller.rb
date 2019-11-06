1b2a75ca4bbf1b11785201f0e62012c429853de0
Error4577 <donttreadonme4577@gmail.com>
2019-11-06 20:07:21 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,8 @@ class ApplicationController < ActionController::Base
 
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :self_introduction, :sex, :img_name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :self_introduction, :sex, :img_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :self_introduction, :sex, :img_name, :corporation])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :self_introduction, :sex, :img_name, :corporation])
   end
 
   def after_sign_in_path_for(resource)
